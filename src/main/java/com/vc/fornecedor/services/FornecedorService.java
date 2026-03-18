07fa2d77e2225acbef5342e75b6d9d792299379c
vinihcolombo <vinihcolombo@gmail.com>
2026-03-18 09:26:54 -0300

    Criação dos Services

---
+++ src/main/java/com/vc/fornecedor/services/FornecedorService.java
@@ -0,0 +1,40 @@
+package com.vc.fornecedor.services;
+
+import com.vc.fornecedor.models.FornecedorModel;
+import com.vc.fornecedor.repositories.FornecedorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class FornecedorService {
+
+    @Autowired
+    private FornecedorRepository fornecedorRepository;
+
+    public List<FornecedorModel> findAll(){
+        return fornecedorRepository.findAll();
+    }
+
+    public FornecedorModel criarFornecedor(FornecedorModel fornecedorModel){
+        return fornecedorRepository.save(fornecedorModel);
+    }
+
+    public void deletarFornecedor(Long id){
+        fornecedorRepository.deleteById(id);
+    }
+
+    public Optional<FornecedorModel> buscarPorId(Long id){
+        return fornecedorRepository.findById(id);
+    }
+
+    public FornecedorModel atualizarFornecedor(Long id, FornecedorModel fornecedorModel){
+        FornecedorModel newFornecedorModel = fornecedorRepository.findById(id).get();
+        newFornecedorModel.setContato(fornecedorModel.getContato());
+        newFornecedorModel.setNomeFantasia(fornecedorModel.getNomeFantasia());
+
+        return fornecedorRepository.save(newFornecedorModel);
+    }
+}
